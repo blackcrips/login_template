43bf5188c8e22aae19146a855bf34691f1292b12
blackcrips <89094409+blackcrips@users.noreply.github.com>
2022-09-28 10:08:09 +0800

    adding ip address

--- classes/Model.class.php
+++ classes/Model.class.php
@@ -22,4 +22,12 @@ class Model extends Dbh
             }
         }
     }
+
+    protected function insertLoginAction($credentials)
+    {
+        $sql = "INSERT INTO user_action (`device`,`ip_address`) VALUES (?,?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$credentials[0],$credentials[1]]);
+        return;
+    }
 }
